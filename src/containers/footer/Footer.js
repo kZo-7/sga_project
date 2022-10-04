31e62c8b5225f4352f38731b3e778a06e741c415
kZo-7 <kgkriz.bsn@gmail.com>
2022-10-04 15:33:58 +0300

    sga-project: Building the footer and styling it

--- src/containers/footer/Footer.js
+++ src/containers/footer/Footer.js
@@ -1,8 +1,15 @@
 import React from 'react'
+import { FaRegCopyright } from "react-icons/fa"
 
-function Footer() {
+const Footer = () => {
   return (
-    <div>Footer</div>
+    <div className='gsa__footer gradient__bg-ft'>
+      <div className='gsa__footer-copyright'>
+        <p>
+          Copyright <FaRegCopyright size="11"/> 2022 kZo
+        </p>
+      </div>
+    </div>
   )
 }
 
