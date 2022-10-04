77ab853b7178d42c03d7ead430519455686808fa
kZo-7 <kgkriz.bsn@gmail.com>
2022-10-04 16:00:30 +0300

    sga_project: Building article's webpage, linked it with the main webpage and styling it

--- src/components/article/Article.js
+++ src/components/article/Article.js
@@ -1,9 +1,76 @@
-import React from 'react'
+import { useEffect } from "react";
+import { useLocation, Link } from "react-router-dom";
+import { Body, Footer } from "../../containers";
+import Navbar from "../navbar/Navbar";
 
-function Article() {
-  return (
-    <div>Article</div>
-  )
+
+const Article = () => {
+    const location = useLocation();
+    const { from, message, info } = location.state;//kanoume distracturing gia na einai pio katanohtos o kwdikas se allous
+    console.log("From: ", from);
+    console.log("Message: ", message);
+    console.log("Info: ", info);
+
+    useEffect(() => {
+        window.scrollTo(0, 0)
+      }, [])
+
+    return (
+        <div className="article">
+            <Navbar />
+            <div className="gsa__article section__padding">
+                <h2>Article Info</h2>
+                {info && (
+                    <div className="gsa__article-container">
+                        <hr className="hrz-line"></hr>
+                        <table class="gsa__article-container_table">
+                            <tr>
+                                <th className="table_head">
+                                    Title
+                                </th>
+                                <td>
+                                    {JSON.parse(info).webTitle}
+                                </td>
+                            </tr>
+                            <tr>
+                                <th className="table_head">
+                                    Refers to
+                                </th>
+                                <td>
+                                    {JSON.parse(info).sectionName}
+                                </td>
+                            </tr>
+                            <tr>
+                                <th className="table_head">
+                                    Published at
+                                </th>
+                                <td>
+                                    {JSON.parse(info).webPublicationDate}
+                                </td>
+                            </tr>
+                            <tr>
+                                <th className="table_head">
+                                    URL
+                                </th>
+                                <td>
+                                    <a href="https://www.theguardian.com/us-news/2022/sep/18/the-end-of-the-debate-republicans-midterms">{JSON.parse(info).webUrl}</a>
+                                </td>
+                            </tr>
+                        </table>
+                    </div>
+                )}
+                <div className="gsa__article-button">
+                    <Link to="/">
+                        <button>
+                            Go Back
+                        </button>
+                    </Link>
+                </div>
+            </div>
+            <Body />
+            <Footer />
+        </div>
+    );
 }
-
 export default Article;
+// na to omorfynw me CSS
