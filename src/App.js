060ae31ed8b806073b0b23fd9fc31a7eb4e6de83
kZo-7 <kgkriz.bsn@gmail.com>
2022-10-04 13:43:37 +0300

    sga-project: delete all src files & defining webpage's Layout

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import "./App.css"
+import { useEffect } from "react";
+import { ArticleMenu, Navbar } from "./components";
+import { Header, Body, Footer } from "./containers";
+
+export default function App() {
+  //const App = () => { 
+    useEffect(() => {
+      window.scrollTo(0, 0)
+    }, [])
+
+  return (
+    <div className="App">
+      <Navbar />
+      <Header />
+      <ArticleMenu />
+      <Body />
+      <Footer />
+    </div>
+  )
+}
+//export default App; //export me to onoma tou
